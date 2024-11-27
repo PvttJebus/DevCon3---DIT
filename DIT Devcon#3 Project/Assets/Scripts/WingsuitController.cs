4d3496f3c86b3779cec6d88991d91e3cf8f0288f
TJ <trevorebeckham@gmail.com>
2024-11-27 14:42:06 -0500

    Tweaking wingsuit

--- DIT Devcon#3 Project/Assets/Scripts/WingsuitController.cs
+++ DIT Devcon#3 Project/Assets/Scripts/WingsuitController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 
 [RequireComponent(typeof(CharacterController))]
 #if ENABLE_INPUT_SYSTEM
@@ -12,101 +13,7 @@ using UnityEngine.InputSystem;
 public class WingsuitController : MonoBehaviour
 {
 
-    [Header("Player")]
-    [Tooltip("Move speed of the character in m/s")]
-    public float MoveSpeed = 2.0f;
-
-    [Tooltip("Sprint speed of the character in m/s")]
-    public float SprintSpeed = 5.335f;
-
-    [Tooltip("How fast the character turns to face movement direction")]
-    [Range(0.0f, 0.3f)]
-    public float RotationSmoothTime = 0.12f;
-
-    [Tooltip("Acceleration and deceleration")]
-    public float SpeedChangeRate = 10.0f;
-
-    public AudioClip LandingAudioClip;
-    public AudioClip[] FootstepAudioClips;
-    [Range(0, 1)] public float FootstepAudioVolume = 0.5f;
-
-    [Space(10)]
-    [Tooltip("The height the player can jump")]
-    public float JumpHeight = 1.2f;
-
-    [Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
-    public float Gravity = -15.0f;
-
-    [Space(10)]
-    [Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
-    public float JumpTimeout = 0.50f;
-
-    [Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
-    public float FallTimeout = 0.15f;
-
-    //[Header("Player Grounded")]
-    //[Tooltip("If the character is grounded or not. Not part of the CharacterController built in grounded check")]
-    //public bool Grounded = true;
-
-    [Tooltip("Useful for rough ground")]
-    public float GroundedOffset = -0.14f;
-
-    [Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
-    public float GroundedRadius = 0.28f;
-
-    [Tooltip("What layers the character uses as ground")]
-    public LayerMask GroundLayers;
-
-    [Header("Cinemachine")]
-    [Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
-    public GameObject CinemachineCameraTarget;
-
-    [Tooltip("How far in degrees can you move the camera up")]
-    public float TopClamp = 70.0f;
-
-    [Tooltip("How far in degrees can you move the camera down")]
-    public float BottomClamp = -30.0f;
-
-    [Tooltip("Additional degress to override the camera. Useful for fine tuning camera position when locked")]
-    public float CameraAngleOverride = 0.0f;
-
-    [Tooltip("For locking the camera position on all axis")]
-    public bool LockCameraPosition = false;
-
-    // cinemachine
-    private float _cinemachineTargetYaw;
-    private float _cinemachineTargetPitch;
-
-    // player
-    private float _speed;
-    private float _animationBlend;
-    private float _targetRotation = 0.0f;
-    private float _rotationVelocity;
-    private float _verticalVelocity;
-    private float _terminalVelocity = 53.0f;
-
-    // timeout deltatime
-    private float _jumpTimeoutDelta;
-    private float _fallTimeoutDelta;
-
-    // animation IDs
-    private int _animIDSpeed;
-    //private int _animIDGrounded;
-    private int _animIDJump;
-    private int _animIDFreeFall;
-    private int _animIDMotionSpeed;
-
-#if ENABLE_INPUT_SYSTEM
-    private PlayerInput _playerInput;
-#endif
-    private Animator _animator;
-    private CharacterController _controller;
-    private StarterAssetsInputs _input;
-    private GameObject _mainCamera;
-
-    private const float _threshold = 0.01f;
-
-    private bool _hasAnimator;
+   
 
     public float speed = 22f;
     public float drag = 8f;
@@ -119,24 +26,14 @@ public class WingsuitController : MonoBehaviour
 
     public float percentage;
 
-    private bool IsCurrentDeviceMouse
-    {
-        get
-        {
-#if ENABLE_INPUT_SYSTEM
-            return _playerInput.currentControlScheme == "KeyboardMouse";
-#else
-				return false;
-#endif
-        }
-    }
-
     private void Start()
     {
+       
         rb = GetComponent<Rigidbody>();
         rotation = transform.eulerAngles;
         tp = rb.GetComponent<ThirdPersonController>();
         isGliding = false;
+
     }
 
     private void Update()
@@ -149,112 +46,60 @@ public class WingsuitController : MonoBehaviour
         //    kyleBody.gameObject.transform.eulerAngles = currentrotation;
         //    isGliding = false;
         //}
-        if (Input.GetKeyUp(KeyCode.Space) == true)
+        if (Input.GetKeyUp(KeyCode.Space) == true && isGliding == false)
         {
-            //tp.enabled = false;
+            tp.enabled = false;
             Vector3 currentrotation = kyleBody.gameObject.transform.eulerAngles;
             currentrotation.x += 85f;
             kyleBody.gameObject.transform.eulerAngles = currentrotation;
             isGliding = true;
+            
+            rb.useGravity = true;
         }
 
         if (isGliding == true)
         {
 
-            //player rotation - X axis
-            rotation.x += 20 * Input.GetAxis("Vertical") * Time.deltaTime;
-            rotation.x = Mathf.Clamp(rotation.x, -45, 45);
-            //player rotation - Y Axis
-            rotation.y += 20 * Input.GetAxis("Horizontal") * Time.deltaTime;
-
-
-            //player rotation - Z axis
-            rotation.z = -5 * Input.GetAxis("Horizontal");
-            rotation.z = Mathf.Clamp(rotation.z, -5, 5);
-            //Transform
-            transform.rotation = Quaternion.Euler(rotation);
-
-            percentage = rotation.x / 45;
-            //Drag: Fast = 4, slow = 6 (i guess the float?)
-            float temp_drag = (percentage * -2) + 6;
-            //Speed: fast = 13.8, Slow (12.5)
-            float temp_speed = percentage * (13.8f - 12.5f) + 12.5f;
-
-            rb.drag = temp_drag;
-            Vector3 localvelocity = transform.InverseTransformDirection(rb.velocity);
-            localvelocity.z = temp_speed;
-            localvelocity = localvelocity * (1 - Time.deltaTime * temp_drag);
-            rb.velocity = transform.TransformDirection(localvelocity);
+            Gliding();
         }
-        Move();
+
+        else
+        {
+            Move();
+        }
+    }
+
+    private void Gliding()
+    {
+        //player rotation - X axis
+        rotation.x += 20 * Input.GetAxis("Vertical") * Time.deltaTime;
+        rotation.x = Mathf.Clamp(rotation.x, -45, 45);
+        //player rotation - Y Axis
+        rotation.y += 20 * Input.GetAxis("Horizontal") * Time.deltaTime;
+
+
+        //player rotation - Z axis
+        rotation.z = -5 * Input.GetAxis("Horizontal");
+        rotation.z = Mathf.Clamp(rotation.z, -5, 5);
+        //Transform
+        transform.rotation = Quaternion.Euler(rotation);
+
+        percentage = rotation.x / 45;
+        //Drag: Fast = 4, slow = 6 (i guess the float?)
+        float temp_drag = (percentage * -2) + 8;
+        //Speed: fast = 13.8, Slow (12.5)
+        float temp_speed = percentage * (22f - 19f) + 19f;
+
+        rb.drag = temp_drag;
+        Vector3 localvelocity = transform.InverseTransformDirection(rb.velocity);
+        localvelocity.z = temp_speed;
+        localvelocity = localvelocity * (1 - Time.deltaTime * temp_drag);
+        rb.velocity = transform.TransformDirection(localvelocity);
     }
 
     private void Move()
     {
-        // set target speed based on move speed, sprint speed and if sprint is pressed
-        float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
-
-        // a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
-
-        // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
-        // if there is no input, set the target speed to 0
-        if (_input.move == Vector2.zero) targetSpeed = 0.0f;
-
-        // a reference to the players current horizontal velocity
-        float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
-
-        float speedOffset = 0.1f;
-        float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
-
-        // accelerate or decelerate to target speed
-        if (currentHorizontalSpeed < targetSpeed - speedOffset ||
-            currentHorizontalSpeed > targetSpeed + speedOffset)
-        {
-            // creates curved result rather than a linear one giving a more organic speed change
-            // note T in Lerp is clamped, so we don't need to clamp our speed
-            _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude,
-                Time.deltaTime * SpeedChangeRate);
-
-            // round speed to 3 decimal places
-            _speed = Mathf.Round(_speed * 1000f) / 1000f;
-        }
-        else
-        {
-            _speed = targetSpeed;
-        }
-
-        _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
-        if (_animationBlend < 0.01f) _animationBlend = 0f;
-
-        // normalise input direction
-        Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
-
-        // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
-        // if there is a move input rotate player when the player is moving
-        if (_input.move != Vector2.zero)
-        {
-            _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
-                              _mainCamera.transform.eulerAngles.y;
-            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
-                RotationSmoothTime);
-
-            // rotate to face input direction relative to camera position
-            transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
-        }
-
-
-        Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
-
-        // move the player
-        _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) +
-                         new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
-
-        // update animator if using character
-        if (_hasAnimator)
-        {
-            _animator.SetFloat(_animIDSpeed, _animationBlend);
-            _animator.SetFloat(_animIDMotionSpeed, inputMagnitude);
-        }
+     
     }
 }
 
